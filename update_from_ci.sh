f0cf103736c9f63be91c95a0e689c6d31fb98bb8
Zoff <zoff99@users.noreply.github.com>
2017-09-18 20:00:14 +0200

    Create update_from_ci.sh

---
+++ update_from_ci.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#####################################################
+# update utox binary from Circle CI (raspi branch)
+#####################################################
+
+cd $(dirname "$0")
+
+pkill toxcam_static # will stop toxcam
+cp -av toxcam_static toxcam_static__BACKUP
+wget -O toxcam_static 'https://circleci.com/api/v1/project/zoff99/ToxCam/latest/artifacts/0/$CIRCLE_ARTIFACTS/RASPI/toxcam_static?filter=successful&branch=master'
+chmod u+rwx toxcam_static
