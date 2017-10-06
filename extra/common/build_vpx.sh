ee06896a9a6238848eac940caee831bd4709cee8
Zoff <zoff99@users.noreply.github.com>
2017-10-06 23:03:41 +0200

    Update build_vpx.sh

--- extra/common/build_vpx.sh
+++ extra/common/build_vpx.sh
@@ -2,12 +2,13 @@
 
 # install libvpx, needed for video encoding/decoding
 if ! [ -d libvpx ]; then
-  git clone --depth=1 --branch=v1.6.1 https://github.com/webmproject/libvpx.git
+  git clone --depth=1 --branch=v1.6.0 https://github.com/webmproject/libvpx.git
 fi
 cd libvpx
 git rev-parse HEAD > libvpx.sha
 if ! ([ -f "${CACHE_DIR}/libvpx.sha" ] && diff "${CACHE_DIR}/libvpx.sha" libvpx.sha); then
-  CROSS=i586-mingw32msvc-  ./configure --target=x86-win32-gcc \
+  # CROSS=i586-mingw32msvc-  ./configure --target=x86-win32-gcc \
+  CROSS=x86_64-w64-mingw32-  ./configure --target=x86_64-win64-gcc
               --prefix="${CACHE_DIR}/usr" \
               --enable-static \
               --disable-examples \
