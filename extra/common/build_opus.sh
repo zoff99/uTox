51e91e9ec3506caa87abe2db6711f706ac407d88
Zoff <zoff99@users.noreply.github.com>
2017-10-26 19:56:43 +0200

    download source from github

--- extra/common/build_opus.sh
+++ extra/common/build_opus.sh
@@ -4,7 +4,8 @@ OPUS_VERSION="1.2.1"
 
 # install libopus, needed for audio encoding/decoding
 if ! [ -f "$CACHE_DIR/usr/lib/pkgconfig/opus.pc" ]; then
-  curl https://ftp.osuosl.org/pub/xiph/releases/opus/opus-${OPUS_VERSION}.tar.gz -o opus.tar.gz
+  curl https://github.com/xiph/opus/releases/download/v1.1.2/opus-1.1.2.tar.gz -o opus.tar.gz
+  # curl https://ftp.osuosl.org/pub/xiph/releases/opus/opus-${OPUS_VERSION}.tar.gz -o opus.tar.gz
   tar xzf opus.tar.gz
   cd opus-${OPUS_VERSION}
   ./configure "$TARGET_HOST" \
