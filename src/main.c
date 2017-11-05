90c07648c7c70dc1aa54bbe8e0996df6eae03be1
Zoff <zoff99@users.noreply.github.com>
2017-11-05 17:57:34 +0100

    tweak video quality

--- src/main.c
+++ src/main.c
@@ -291,10 +291,10 @@ void utox_init(void) {
 #else
 // high quality ---
 	global__MAX_DECODE_TIME_US = 1;
-	global__VP8E_SET_CPUUSED_VALUE = 8;
+	global__VP8E_SET_CPUUSED_VALUE = 4;
 	global__VPX_END_USAGE = 3;
 	global__VPX_KF_MAX_DIST = 48;
-	global__VPX_G_LAG_IN_FRAMES = 3;
+	global__VPX_G_LAG_IN_FRAMES = 0;
 // high quality ---
 #endif
 
