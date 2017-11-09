ab718b06d64966c9e1ca34fd75bdb16233e2a7f3
Zoff <zoff99@users.noreply.github.com>
2017-11-09 21:49:16 +0100

    tweak

--- extra/gitlab/windows-script.sh
+++ extra/gitlab/windows-script.sh
@@ -3,7 +3,7 @@ set -eux
 
 . ./extra/gitlab/env.sh
 
-export CFLAGS=" -g -O3 -I$CACHE_DIR/usr/include -I/usr/share/mingw-w64/include/ "
+export CFLAGS=" -g -I$CACHE_DIR/usr/include -I/usr/share/mingw-w64/include/ "
 
 # CMake 3.2 or higher is required. for c-toxcore :-(
 sudo apt-get install cmake3
@@ -11,7 +11,6 @@ sudo apt-get install cmake3
 mkdir build_win
 cd build_win
 
-#    -DCMAKE_BUILD_TYPE=Debug \
 cmake .. \
     -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-win64.cmake \
     -DCMAKE_INCLUDE_PATH="$CACHE_DIR/usr/include" \
