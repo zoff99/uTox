e5fca189ac765bb5c29a887da4e5d0ea3596bd97
zoff99 <zoff@zoff.cc>
2017-11-19 16:37:42 +0100

    tweak video settings

--- src/main.c
+++ src/main.c
@@ -300,9 +300,9 @@ void utox_init(void) {
 	global__MAX_DECODE_TIME_US = 1;
 	global__VP8E_SET_CPUUSED_VALUE = 5;
 	global__VPX_END_USAGE = 3; // constant bitrate
-	global__VPX_KF_MAX_DIST = 60;
+	global__VPX_KF_MAX_DIST = 20;
 	global__VPX_G_LAG_IN_FRAMES = 0;
-	UTOX_DEFAULT_BITRATE_V = 20000;
+	UTOX_DEFAULT_BITRATE_V = 1000;
 
     global__VPX_ENCODER_USED = 1; // 0 -> VP8, 1 -> VP9
     global__VPX_DECODER_USED = 0; // 0 -> VP8, 1 -> VP9
