519cb1d127b107744a856fcd1d578ae4365d18a5
zoff99 <zoff@zoff.cc>
2017-11-19 16:39:16 +0100

    LOG_LVL_INFO

--- src/settings.c
+++ src/settings.c
@@ -77,7 +77,7 @@ SETTINGS settings = {
     .status_notifications   = true,
     .group_notifications    = GNOTIFY_ALWAYS,
 
-    .verbose = LOG_LVL_ERROR,
+    .verbose = LOG_LVL_INFO,
     .debug_file = NULL,
 
     // .theme                       // included here to match the full struct
