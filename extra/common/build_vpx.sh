b1330f8bffc8befb41533f16b82f6521378c11a7
zoff99 <zoff@zoff.cc>
2018-02-17 21:13:42 +0100

    tweak vpx compile options

--- extra/common/build_vpx.sh
+++ extra/common/build_vpx.sh
@@ -3,7 +3,7 @@
 # install libvpx, needed for video encoding/decoding
 if ! [ -d libvpx ]; then
   # git clone --depth=1 --branch=v1.5.0 https://github.com/webmproject/libvpx.git
-  git clone --depth=1 --branch=v1.6.1 https://github.com/webmproject/libvpx.git
+  git clone --depth=1 --branch=v1.7.0 https://github.com/webmproject/libvpx.git
   # git clone --depth=1 --branch=master https://github.com/webmproject/libvpx.git
 fi
 cd libvpx
@@ -19,14 +19,18 @@ if ! ([ -f "${CACHE_DIR}/libvpx.sha" ] && diff "${CACHE_DIR}/libvpx.sha" libvpx.
   CROSS=x86_64-w64-mingw32-  ./configure --target=x86_64-win64-gcc \
               --prefix="${CACHE_DIR}/usr" \
               --enable-static \
-              --size-limit=16384x16384 \
               --disable-shared \
               --disable-unit-tests \
-              --enable-postproc \
+              --size-limit=16384x16384 \
+              --enable-onthefly-bitpacking \
+              --enable-runtime-cpu-detect \
               --enable-multi-res-encoding \
-              --enable-temporal-denoising \
-              --enable-vp9-temporal-denoising \
+              --enable-error-concealment \
+              --enable-better-hw-compatibility \
+              --enable-postproc \
               --enable-vp9-postproc
+              --enable-temporal-denoising \
+              --enable-vp9-temporal-denoising 
 
   mkdir ../libvpx-test-data
   # LIBVPX_TEST_DATA_PATH=../libvpx-test-data make testdata
