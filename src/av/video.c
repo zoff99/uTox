6f8ddb322ef7f5efcfc03e4577a0f6f136cb7964
zoff99 <zoff@zoff.cc>
2019-07-08 20:20:20 +0200

    optimize mem copy a little bit

--- src/av/video.c
+++ src/av/video.c
@@ -706,7 +706,7 @@ void scale_down_yuv420_image(uint8_t *old_y, uint8_t *old_u, uint8_t *old_v,
         }
     }
 
-    // scale down U layer
+    // scale down U + V layer
     for (int y = 0; y != (new_height / 2); y++) {
         const int y0 = y * (old_height / 2) / (new_height / 2);
         for (int x = 0; x != (new_width / 2); x++) {
@@ -715,9 +715,11 @@ void scale_down_yuv420_image(uint8_t *old_y, uint8_t *old_u, uint8_t *old_v,
             const int a         = x + y * (new_width / 2);
             const int b         = x0 + y0 * (old_width / 2);
             new_u[a]            = old_u[b];
+            new_v[a]            = old_v[b];
         }
     }
 
+#if 0
     // scale down V layer
     for (int y = 0; y != (new_height / 2); y++) {
         const int y0 = y * (old_height / 2) / (new_height / 2);
@@ -726,9 +728,9 @@ void scale_down_yuv420_image(uint8_t *old_y, uint8_t *old_u, uint8_t *old_v,
 
             const int a         = x + y * (new_width / 2);
             const int b         = x0 + y0 * (old_width / 2);
-            new_v[a]            = old_v[b];
         }
     }
+#endif
 
 }
 
